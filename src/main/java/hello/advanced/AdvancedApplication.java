f779a8abceb3b6ce6e1db7d4e35d1aa92153f739
danghunri <sanghoon0120@gmail.com>
2024-12-29 23:26:50 +0900

    feat: 예제 프로젝트 만들기 v2

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -1,11 +1,13 @@
 package hello.advanced;
 
 import hello.advanced.config.AppV1Config;
+import hello.advanced.config.AppV2Config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
-@Import(AppV1Config.class)
+//@Import(AppV1Config.class)
+@Import({AppV1Config.class ,AppV2Config.class})
 @SpringBootApplication(scanBasePackages = "hello.advanced.app2")
 public class AdvancedApplication {
 
---
+++ src/main/java/hello/advanced/app2/v2/OrderControllerV2.java
@@ -0,0 +1,30 @@
+package hello.advanced.app2.v2;
+
+import hello.advanced.app2.v1.OrderServiceV1;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+public class OrderControllerV2 {
+    private final OrderServiceV2 orderService;
+
+    public OrderControllerV2(OrderServiceV2 orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping("/v2/request")
+    public String request(String itemId) {
+        orderService.orderItem(itemId);
+        return "ok";
+    }
+
+    @GetMapping("/v2/no-log")
+    public String noLog() {
+        return "ok";
+    }
+}
