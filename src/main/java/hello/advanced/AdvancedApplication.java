72401b49406ee30e317b41d88c6abe3bcd0c083a
danghunri <sanghoon0120@gmail.com>
2025-01-21 22:47:48 +0900

    feat: 프록시 팩토리 - 적용2

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -7,6 +7,7 @@ import hello.advanced.config.v1_proxy.InterfaceProxyConfig;
 import hello.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.advanced.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.advanced.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -20,7 +21,8 @@ import org.springframework.context.annotation.Import;
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app2")
 public class AdvancedApplication {
 
