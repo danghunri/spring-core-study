1191bcfa08ba18bc734bd98c408f30a0e2e675e5
danghunri <sanghoon0120@gmail.com>
2025-01-30 01:44:56 +0900

    feat: 스프링이 제공하는 빈 후처리기2

--- src/main/java/hello/advanced/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/advanced/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.advanced.config.AppV2Config;
 import hello.advanced.config.v3_proxyfactory.advice.LogTraceAdvice;
 import hello.advanced.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -15,10 +16,30 @@ import org.springframework.context.annotation.Import;
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
 
-    @Bean
-    public Advisor getAdvisor1(LogTrace logTrace) {
+//    @Bean
+    public Advisor advisor1(LogTrace logTrace) {
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace)
+    {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.advanced.app2..*(..))");
+
+        LogTraceAdvice advice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, advice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace)
+    {
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.advanced.app2..*(..)) && !execution(* hello.advanced.app2..noLog(..))");
 
         LogTraceAdvice advice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut, advice);
