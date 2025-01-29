0951547c760e510af9afce6a783741e6fe31b252
danghunri <sanghoon0120@gmail.com>
2025-01-30 01:36:11 +0900

    feat: 스프링이 제공하는 빈 후처리기1

--- src/main/java/hello/advanced/AdvancedApplication.java
+++ src/main/java/hello/advanced/AdvancedApplication.java
@@ -8,6 +8,7 @@ import hello.advanced.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import hello.advanced.config.v2_dynamicproxy.DynamicProxyFilterConfig;
 import hello.advanced.config.v3_proxyfactory.ProxyFactoryConfigV1;
 import hello.advanced.config.v3_proxyfactory.ProxyFactoryConfigV2;
+import hello.advanced.config.v5_autoproxy.AutoProxyConfig;
 import hello.advanced.trace.logtrace.LogTrace;
 import hello.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -22,7 +23,8 @@ import org.springframework.context.annotation.Import;
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyFilterConfig.class)
 //@Import(ProxyFactoryConfigV1.class)
-@Import(ProxyFactoryConfigV2.class)
+//@Import(ProxyFactoryConfigV2.class)
+@Import(AutoProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.advanced.app2")
 public class AdvancedApplication {
 
